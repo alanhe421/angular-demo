11be34643478c5a0bf071fdd1f28401447ed3297
alanhg <qianghe421@163.com>
2017-08-12 16:26:52 +0800

    optimize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,60 @@
 import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
-import {HttpModule, Http} from "@angular/http";
+import {Http, HttpModule} from "@angular/http";
 import {AppComponent} from "./app.component";
-import {TranslateModule, TranslateLoader} from "@ngx-translate/core";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
 import {TranslateHttpLoader} from "@ngx-translate/http-loader";
-import { I18nComponent } from './i18n/i18n.component';
-import { MyhighlightDirective } from './myhighlight.directive';
-import { ChildComponent } from './child/child.component';
-import { NodataComponent } from './nodata/nodata.component';
+import {MyhighlightDirective} from "./myhighlight.directive";
+import {ChildComponent} from "./child/child.component";
+import {NodataComponent} from "./nodata/nodata.component";
 import {BsDropdownModule} from "ngx-bootstrap";
 import {InfiniteScrollDirective} from "./infinite-scroll.directive";
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
+import {RouterModule, Routes} from "@angular/router";
+import {DocsComponent} from "./docs/docs.component";
 
 export function createTranslateLoader(http: Http) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
-
+const appRoutes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    // {path: 'home', loadChildren: './i18n/i18n.module#I18nModule'},
+    // {path: 'i18n', loadChildren: './i18n/i18n.module#I18nModule'},
+    {path: 'docs', component: DocsComponent},
+];
 @NgModule({
-  declarations: [
-    AppComponent,
-    I18nComponent,
-    MyhighlightDirective,
-    InfiniteScrollDirective,
-    ChildComponent,
-    NodataComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: (createTranslateLoader),
-        deps: [Http]
-      }
-    }),
-    BsDropdownModule.forRoot()
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MyhighlightDirective,
+        InfiniteScrollDirective,
+        ChildComponent,
+        NodataComponent,
+        HeaderComponent,
+        FooterComponent,
+        DocsComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot(appRoutes),
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: (createTranslateLoader),
+                deps: [Http]
+            }
+        }),
+        BsDropdownModule.forRoot()
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
--- src/app/i18n/i18n.module.ts
+++ src/app/i18n/i18n.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {I18nComponent} from "./i18n.component";
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [I18nComponent]
 })
 export class I18nModule { }
