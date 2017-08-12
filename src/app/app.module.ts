a6192bafaaba70258bd61cb3753ec17f78f60769
alanhg <qianghe421@163.com>
2017-08-12 17:17:22 +0800

    ng-content活用-demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {HeaderComponent} from "./header/header.component";
 import {FooterComponent} from "./footer/footer.component";
 import {RouterModule, Routes} from "@angular/router";
 import {DocsComponent} from "./docs/docs.component";
+import {CardComponent} from "./docs/card/card.component";
 
 export function createTranslateLoader(http: Http) {
     return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -37,7 +38,8 @@ const appRoutes: Routes = [
         NodataComponent,
         HeaderComponent,
         FooterComponent,
-        DocsComponent
+        DocsComponent,
+        CardComponent
     ],
     imports: [
         BrowserModule,
--- src/app/docs/docs.component.ts
+++ src/app/docs/docs.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 
 @Component({
-  selector: 'app-docs',
-  templateUrl: './docs.component.html',
-  styleUrls: ['./docs.component.css']
+    selector: 'app-docs',
+    templateUrl: './docs.component.html',
+    styleUrls: ['./docs.component.css']
 })
 export class DocsComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
