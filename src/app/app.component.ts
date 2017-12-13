a589f9c5741d797755e74e515dbe1438e982c844
alanhg <qianghe421@163.com>
2017-12-13 15:48:24 +0800

    路由切换，滚动到顶部

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,45 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+    title = 'app works!';
 
-  loadMore(): void {
-    // alert('加载更多');
-  }
+    constructor(private router: Router) {
 
-  /**
-   * 搜索
-   */
-  search() {
-    alert('searching!')
-  }
+    }
+
+    ngOnInit() {
+        this.router.events.subscribe((evt) => {
+            if (!(evt instanceof NavigationEnd)) {
+                return;
+            }
+            window.scrollTo(0, 0);
+        });
+    }
+
+    loadMore(): void {
+        // alert('加载更多');
+    }
+
+    /**
+     * 搜索
+     */
+    search() {
+        alert('searching!');
+    }
+
+    onActivate(e, outlet) {
+        outlet.scrollTop = 0;
+    }
+
+    onDeactivate() {
+        document.body.scrollTop = 0;
+    }
 }
 
 
