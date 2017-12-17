1243913e5fb41c93aa5967faa9469c028c74b530
alanhg <qianghe421@163.com>
2017-12-17 22:06:40 +0800

    连线OK

--- src/app/jsplumb/jsplumb.component.ts
+++ src/app/jsplumb/jsplumb.component.ts
@@ -55,7 +55,7 @@ export class JsplumbComponent implements OnInit {
             ],
             Container: 'canvas'
         });
-        var basicType = {
+        const basicType = {
             connector: 'StateMachine',
             paintStyle: {stroke: 'red', strokeWidth: 4},
             hoverPaintStyle: {stroke: 'blue'},
@@ -72,15 +72,20 @@ export class JsplumbComponent implements OnInit {
         this.jsPlumbInstance.draggable('flowchartWindow3');
         this.jsPlumbInstance.draggable('flowchartWindow4');
 
-        this.jsPlumbInstance.connect({
-            source: 'flowchartWindow1', // 源端点
-            target: 'flowchartWindow2', // 目标端点
-            editable: true
-        });
+        // this.jsPlumbInstance.connect({
+        //     source: 'flowchartWindow1', // 源端点
+        //     target: 'flowchartWindow2', // 目标端点
+        //     editable: true
+        // });
+        const anchors = [[1, 0.2, 1, 0], [0.8, 1, 0, 1], [0, 0.8, -1, 0], [0.2, 0, 0, -1]];
 
         // 增加端点
-        this.jsPlumbInstance.addEndpoint('flowchartWindow1');
-        this.jsPlumbInstance.addEndpoint('flowchartWindow2');
+        this.jsPlumbInstance.addEndpoint('flowchartWindow1', {anchor: anchors}, sourceEndpoint);
+        this.jsPlumbInstance.addEndpoint('flowchartWindow1', targetEndpoint);
+
+
+        this.jsPlumbInstance.addEndpoint('flowchartWindow2', targetEndpoint);
+        this.jsPlumbInstance.addEndpoint('flowchartWindow2', targetEndpoint);
 
 
         // listen for clicks on connections, and offer to delete connections on click.
@@ -107,3 +112,60 @@ export class JsplumbComponent implements OnInit {
 
 
 }
+
+
+const connectorPaintStyle = {
+        strokeWidth: 2,
+        stroke: '#61B7CF',
+        joinstyle: 'round',
+        outlineStroke: 'white',
+        outlineWidth: 2
+    },
+    // .. and this is the hover style.
+    connectorHoverStyle = {
+        strokeWidth: 3,
+        stroke: '#216477',
+        outlineWidth: 5,
+        outlineStroke: 'white'
+    },
+    endpointHoverStyle = {
+        fill: '#216477',
+        stroke: '#216477'
+    },
+
+// the definition of source endpoints (the small blue ones)
+    sourceEndpoint = {
+        endpoint: 'Dot',
+        paintStyle: {
+            stroke: '#7AB02C',
+            fill: 'transparent',
+            radius: 7,
+            strokeWidth: 1
+        },
+        isSource: true,
+        connector: ['Flowchart', {stub: [40, 60], gap: 10, cornerRadius: 5, alwaysRespectStubs: true}],
+        connectorStyle: connectorPaintStyle,
+        hoverPaintStyle: endpointHoverStyle,
+        connectorHoverStyle: connectorHoverStyle,
+        dragOptions: {},
+        overlays: [
+            ['Label', {
+                location: [0.5, 1.5],
+                label: 'Drag',
+                cssClass: 'endpointSourceLabel',
+                visible: false
+            }]
+        ]
+    },
+    // the definition of target endpoints (will appear when the user drags a connection)
+    targetEndpoint = {
+        endpoint: 'Dot',
+        paintStyle: {fill: '#7AB02C', radius: 7},
+        hoverPaintStyle: endpointHoverStyle,
+        maxConnections: -1,
+        dropOptions: {hoverClass: 'hover', activeClass: 'active'},
+        isTarget: true,
+        overlays: [
+            ['Label', {location: [0.5, -0.5], label: 'Drop', cssClass: 'endpointTargetLabel', visible: false}]
+        ]
+    };
