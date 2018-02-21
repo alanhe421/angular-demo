16824b96cd97fba22e4b146a7a5a3d30e95d4a0c
alanhg <qianghe421@163.com>
2018-02-21 11:18:14 +0800

    fixed #39

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
 
 if (environment.production) {
-  enableProdMode();
+    // add Google Analytics script to <head>
+    const script1 = document.createElement('script');
+    script1.async = true;
+    script1.src = 'https://www.googletagmanager.com/gtag/js?id=UA-xxxxxxx-1';
+    const script2 = document.createElement('script');
+    script2.innerHTML = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-xxxxxxx-1');
+  `;
+    document.head.appendChild(script1);
+    document.head.appendChild(script2);
+
+    enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
