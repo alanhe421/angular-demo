cfbbaca951b9cc0474e787b83881792f99789fcb
alanhg <qianghe421@163.com>
2018-05-14 22:52:37 +0800

    optimize

--- src/app/home/modal/modal.component.spec.ts
+++ src/app/home/modal/modal.component.spec.ts
@@ -1,6 +1,6 @@
-import {async, ComponentFixture, TestBed} from "@angular/core/testing";
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import {ModalComponent} from "./modal.component";
+import {ModalComponent} from './modal.component';
 
 describe('ModalComponent', () => {
     let component: ModalComponent;
