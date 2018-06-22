b6e13934864b173ae308e2deeadccbc797eb2f58
alanhg <qianghe421@163.com>
2018-06-22 21:42:02 +0800

    社交分享增加微博分享

--- src/app/news/social-share/social-share.component.ts
+++ src/app/news/social-share/social-share.component.ts
@@ -3,6 +3,11 @@ import {ModalDirective} from 'ngx-bootstrap';
 
 declare const QRCode: any;
 
+export interface ShareConfig {
+    url: string;
+    title: string;
+}
+
 @Component({
     selector: 'app-social-share',
     templateUrl: './social-share.component.html',
@@ -11,13 +16,11 @@ declare const QRCode: any;
 export class SocialShareComponent implements OnInit, OnDestroy {
     @ViewChild('qrcodeModal') qrcodeModal: ModalDirective;
     @ViewChild('qrcode') qrcodeElement: ElementRef;
-    @Input('link') shareLink: string;
+    @Input('config') shareConfig: ShareConfig;
     qrcode: any;
 
     constructor() {
-        if (!this.shareLink) {
-            this.shareLink = location.href;
-        }
+
     }
 
     ngOnInit() {
@@ -27,16 +30,21 @@ export class SocialShareComponent implements OnInit, OnDestroy {
     showQrcode() {
         this.qrcodeModal.show();
         if (this.qrcode) {
-            this.qrcode.makeCode(this.shareLink); // make another code.
+            this.qrcode.makeCode(this.shareConfig.url); // make another code.
         } else {
             this.qrcode = new QRCode(this.qrcodeElement.nativeElement, {
-                text: this.shareLink,
+                text: this.shareConfig.url,
                 width: 300,
                 height: 300,
                 colorDark: '#000000',
                 colorLight: '#ffffff'
             });
         }
+    }
+
+
+    goToWeibo() {
+
     }
 
     ngOnDestroy() {
